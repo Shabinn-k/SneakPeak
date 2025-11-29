172daddee55102041cb8d3aebba6c2682f2f9130
Shabinn-k <shhabin.07@gmail.com>
2025-11-29 12:03:29 +0530

    created project and folderstructure

---
+++ frontend/src/MainRouter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Routes, Route } from 'react-router-dom'
+
+const MainRouter = () => {
+
+  return (
+    
+    <div>MainRouter</div>
+  
+)
+
+}
+
+export default MainRouter
